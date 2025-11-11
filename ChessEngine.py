e5b4065da86d282dbd717d39a037d0fe32cd1584
Emaad Qazi <qaziemaad@gmail.com>
2025-11-11 14:18:06 -0500

    Got basic moving functionality down, need to add rules and elligible moves next

--- ChessEngine.py
+++ ChessEngine.py
@@ -22,3 +22,40 @@ class GameState():
         ]
         self.whiteToMove = True
         self.moveLog = []
+        
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = "--" # When we move, space we moved from becomes empty
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move) # Log the move so we can undo it later, or display history
+        self.whiteToMove = not self.whiteToMove # Swap players
+        
+class Move():
+    
+    # Dictionary, maps keys to values -> key : value 
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4,
+                   "5": 3, "6": 2, "7": 1, "8": 0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()} # reverses above ^
+    filesToCols =  {"a": 0, "b": 1, "c": 2, "d": 3,
+                  "e": 4, "f": 5, "g": 6, "h": 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()} # reverses above
+    
+    '''
+    start: where player was
+    endSq: where player ends up
+    board: current board state
+    '''
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+        
+    def getChessNotation(self):
+        # Real Chess Notation
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+        
+    def getRankFile(self, r, c):
+        # Rank File Notation
+        return self.colsToFiles[c] + self.rowsToRanks[r]
--- ChessMain.py
+++ ChessMain.py
@@ -33,10 +33,29 @@ def main():
     gs = ChessEngine.GameState() # Calling on constructor
     loadImages() # Only do it once before while loop
     running = True 
+    sqSelected = () # Initially, no square selected -> keep track of last click of user (tuple: (row, col))
+    playerClicks = [] # Keep track of player clicks (two tuples: [(7, 4), (4, 4)]); moving from x,y to x,y
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos() # (x, y) location of the mouse
+                col = location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE
+                if sqSelected == (row, col): # User clicks same square twice
+                    sqSelected = () # Deselect
+                    playerClicks = [] # Clear the player clicks
+                else: # Selecting different square than previously selected
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected) # Append for both 1st and 2nd clicks
+                if len(playerClicks) == 2: # After the 2nd click
+                    move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    print(move.getChessNotation())
+                    gs.makeMove(move)
+                    sqSelected = {} # Reset user clicks so user can make more moves
+                    playerClicks = []
+                    
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
